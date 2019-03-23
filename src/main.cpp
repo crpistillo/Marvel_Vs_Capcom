247d6f0e66296f488f571c93afbccfb6c79e6f19
Sebastian Kissling <sebastian.kissling@gmail.com>
2019-03-23 13:23:21 -0300

    Estructura basica

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : marvel-vs-capcom.cpp
+// Author      : Grupo Capcom
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
