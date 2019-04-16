aa19e5159024152a57f7f83d836c0cf99c670130
paargo <gpabloariel@gmail.com>
2019-04-16 00:29:13 -0300

    Correccion en MCGame

--- src/MCGame.cpp
+++ src/MCGame.cpp
@@ -126,6 +126,9 @@ MCGame::MCGame(Logger* logger, json config){
 	int widthWolverine = wolverineConfig["width"];
 	int heightWolverine = wolverineConfig["height"];
 
+	string spidermanPath = spidermanConfig["filepath"];
+	string wolverinePath = wolverineConfig["filepath"];
+
 
 	int spidermanSobrante = widthSpiderman*242/640;
 	int spidermanAncho= widthSpiderman*110/640;
@@ -135,18 +138,20 @@ MCGame::MCGame(Logger* logger, json config){
 	int INITIAL_POS_X_PLAYER_ONE = ((LEVEL_WIDTH/2)-spidermanSobrante)-(spidermanAncho/2)-200;
 	int INITIAL_POS_X_PLAYER_TWO = ((LEVEL_WIDTH/2)-wolverineSobrante)-(wolverineAncho/2)+200;
 
-	logger->log("Creacion de personajes.", DEBUG);
-
 	Character* character1 = new Spiderman(INITIAL_POS_X_PLAYER_ONE, false, widthSpiderman, heightSpiderman, spidermanSobrante, spidermanAncho);
 	character1->setZIndex(spidermanConfig["zindex"]);
+	character1->setFilepath(spidermanPath);
     Character* character2 = new Wolverine(INITIAL_POS_X_PLAYER_ONE, false, widthWolverine, heightWolverine, wolverineSobrante, wolverineAncho);
     character2->setZIndex(wolverineConfig["zindex"]);
+    character2->setFilepath(wolverinePath);
 
 
     Character* character3 = new Wolverine(INITIAL_POS_X_PLAYER_TWO, true, widthWolverine, heightWolverine, wolverineSobrante, wolverineAncho);
     character3->setZIndex(wolverineConfig["zindex"]);
+    character3->setFilepath(wolverinePath);
     Character* character4 = new Spiderman(INITIAL_POS_X_PLAYER_TWO, true, widthSpiderman, heightSpiderman, spidermanSobrante, spidermanAncho);
     character4->setZIndex(spidermanConfig["zindex"]);
+    character4->setFilepath(spidermanPath);
 
     logger->log("Creacion de controles.", DEBUG);
 
@@ -158,13 +163,9 @@ MCGame::MCGame(Logger* logger, json config){
     player1 = new Player(character1, character2, controlPlayer1);
     player2 = new Player(character3, character4, controlPlayer2);
 
-    logger->log("Definicion de Fondo.", DEBUG);	
-    
     middleGround = new Layer(2400, 600, 3.33, 400);//3.33
     backGround = new Layer(1600,600,6.66667,800);//6.715
     frontGround = new Layer(3200,600,0,0);
-
-    logger->log("Creacion de Parallax.", DEBUG);
 
     parallaxController = new Parallax(&middleGround, &backGround, &camera, &centerBefore, &centerLater);
 }
@@ -190,7 +191,6 @@ void MCGame::run() {
 void orderRenderizableListByZIndex(Renderizable** list);
 
 void MCGame::render() {
-	logger->log("Inicio render.", DEBUG);	
 	SDL_SetRenderDrawColor( m_Renderer, 0xFF, 0xFF, 0xFF, 0xFF );
 	SDL_RenderClear(m_Renderer); // clear the renderer to the draw color
 
@@ -214,7 +214,7 @@ void MCGame::render() {
 			player1->render(m_Renderer, camera.x, camera.y, player2->getCentro());
 		}
 	}
-	logger->log("Fin render.", DEBUG);
+
     SDL_RenderPresent(m_Renderer); // draw to the screen
 }
 
@@ -243,18 +243,14 @@ void orderRenderizableListByZIndex(Renderizable** list){
 
 void MCGame::clean() {
     //m_Texture.free();
-    logger->log("Inicio limpieza MCGame.", INFO);
     delete player1;
     delete player2;
-    logger->log("Borrado de jugadores finalizado.", DEBUG);    
     frontGroundTexture.free();
     middleGroundTexture.free();
     backGroundTexture.free();
-    logger->log("Liberacion de variables de fondo finalizado.", DEBUG);
     delete backGround;
     delete middleGround;
     delete parallaxController;
-    logger->log("Borrado de fondos finalizado.", DEBUG);
     SDL_DestroyWindow(m_Window);
     SDL_DestroyRenderer(m_Renderer);
     SDL_Quit();
@@ -269,7 +265,6 @@ void MCGame::handleEvents() {
 
 void MCGame::update() {
 
-	logger->log("Reubicacion inicio.", DEBUG);
 	//distancia = player1->getPosX() + (198/2) - player2->getPosX() + (157/2);
 	//distancia2 = player2->getPosX() + (157/2) - player1->getPosX() + (198/2);
 	if (player1->getCentro() > player2->getCentro()) {
@@ -279,11 +274,9 @@ void MCGame::update() {
 		distancia = player1->getPosX()+player1->getSobrante() - (player2->getPosX()+player2->getSobrante()+player2->getWidth());
 		distancia2 = player2->getPosX()+player2->getSobrante()+player2->getWidth() - (player1->getPosX()+player1->getSobrante());
 	}
-    logger->log("Actualizacion posicion MCGame.", DEBUG);
     player1->update(m_Renderer, distancia, player2->getCentro());
     player2->update(m_Renderer, distancia2, player1->getCentro());
 
-    logger->log("Actualizacion parallax - MCGame.", DEBUG);
     parallaxController->doParallax(&player1,&player2);
 }
 
@@ -292,7 +285,6 @@ void orderBackgroundsByZIndex(json* backgroundList){
 	int pos_sel = 0;
 	json aux;
 
-//	logger->log("Definicion de ubicacion segun zindex.", INFO);
 	for(int i = 2; i >= 0; i--){
 
 		for(int x = 0; x <= i; x++){
@@ -305,6 +297,5 @@ void orderBackgroundsByZIndex(json* backgroundList){
 		backgroundList[i] = backgroundList[pos_sel];
 		backgroundList[pos_sel] = aux;
 		pos_sel = 0;
-//	logger->log("Fin definicion de ubicacion segun zindex.", INFO);
 	}
 }
