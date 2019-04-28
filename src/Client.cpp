0ab694e3258e0a447b32fed9540056c12491bbf8
Sebastian Kissling <sebastian.kissling@gmail.com>
2019-04-28 16:14:06 -0300

    Separar en otra función el código para la conexión con el sv

--- src/Client.cpp
+++ src/Client.cpp
@@ -37,29 +37,10 @@ json parseConfigFile(string logPath) {
 MCGame* mcGame = 0;
 Logger *Logger::instance = 0;
 
-int initClient(int cantArg,char* dirJson,char * host,char * port,char * cantInst) {
-	Logger* logger = Logger::getInstance();
-	logger->startSession();
-	logger->log("Logger iniciado.", DEBUG);
-	json config;
-
-    if(cantArg != 2){
-    	logger->log("Archivo de configuracion no especificado, cargando el archivo de configuracion por defecto.", INFO);
-    	config = parseConfigFile("config/config_default.json");
-    }
-    else{
-    	string configPath = (string) dirJson;
-    	logger->log("Procediento a utilizar archivo de configuracion especificado: " + configPath , INFO);
-    	config = parseConfigFile(dirJson);
-    }
-	int ancho = config["window"]["width"];
-	int alto = config["window"]["height"];
-	logger->log("Configuracion Cargada - Inicio de Ciclo.", INFO);
-
-	////////////////
-    char buf[1024];
-    char enviar[1024];
-    int Descriptor;
+int connectToServer() {
+	char buf[1024];
+	char enviar[1024];
+	int Descriptor;
 	struct sockaddr_in Direccion;
 	Direccion.sin_family = AF_INET;
 	Direccion.sin_port = htons(8080); //	htons((int)port);
@@ -69,11 +50,13 @@ int initClient(int cantArg,char* dirJson,char * host,char * port,char * cantInst
 	Descriptor = socket (AF_INET, SOCK_STREAM, 0);
 	if(Descriptor == -1) {
 		printf ("Error en apertura de socket servidor\n");
-    } else {
-    	int estado_conexion = connect(Descriptor,(struct sockaddr *)&Direccion, sizeof (Direccion));
-    	if(estado_conexion == -1) {
-    		printf("Hubo un error en la conexion remota\n");
-    	} else {
+		return 1;
+	} else {
+		int estado_conexion = connect(Descriptor,(struct sockaddr *)&Direccion, sizeof (Direccion));
+		if(estado_conexion == -1) {
+			printf("Hubo un error en la conexion remota\n");
+			return 1;
+		} else {
 			while(1) {
 				//El servidor espera el primer mensaje
 				printf("Escribir mensaje: ");
@@ -93,19 +76,49 @@ int initClient(int cantArg,char* dirJson,char * host,char * port,char * cantInst
 			}
 
 			close(Descriptor);
-    	}
-    }
-	//////////////////////
+		}
+	}
+	return 0;
+}
 
-    mcGame = new MCGame(config, ancho, alto);
-    mcGame->camera = { 0, 0, ancho, alto };
-    mcGame->init("Marvel vs Capcom", 100, 100, ancho, alto, 0);
-    mcGame->run();
+void cleanLogger(Logger* logger) {
+	logger->finishSession();
+	delete logger;
+}
+
+int initClient(int cantArg,char* dirJson,char * host,char * port,char * cantInst) {
+	Logger* logger = Logger::getInstance();
+	logger->startSession();
+	logger->log("Logger iniciado.", DEBUG);
+	json config;
+
+    if(cantArg != 2){
+    	logger->log("Archivo de configuracion no especificado, cargando el archivo de configuracion por defecto.", INFO);
+    	config = parseConfigFile("config/config_default.json");
+    }
+    else{
+    	string configPath = (string) dirJson;
+    	logger->log("Procediento a utilizar archivo de configuracion especificado: " + configPath , INFO);
+    	config = parseConfigFile(dirJson);
+    }
+	int ancho = config["window"]["width"];
+	int alto = config["window"]["height"];
+	logger->log("Configuracion Cargada - Inicio de Ciclo.", INFO);
+
+	if(connectToServer() == 1) {
+		logger->log("No se pudo hacer la conexión con el servidor.", ERROR);
+		cleanLogger(logger);
+		return 0;
+	}
+
+	mcGame = new MCGame(config, ancho, alto);
+	mcGame->camera = { 0, 0, ancho, alto };
+	mcGame->init("Marvel vs Capcom", 100, 100, ancho, alto, 0);
+	mcGame->run();
 
     mcGame->clean();
     logger->log("Fin de ciclo de ejecucion.", INFO);
-    logger->finishSession();
-    delete logger;
+    cleanLogger(logger);
     delete mcGame;
     return 0;
 }
