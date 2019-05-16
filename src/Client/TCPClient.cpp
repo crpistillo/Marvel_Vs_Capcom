93147f727e5e91789d325556e8b78ff9aa429847
IgVelasco <iignaciovelasco@gmail.com>
2019-05-16 12:19:13 -0300

    Minor changes client

--- src/Client/TCPClient.cpp
+++ src/Client/TCPClient.cpp
@@ -3,68 +3,34 @@
 //
 
 #include "TCPClient.h"
+#include "../tools/logger/Logger.h"
 
 TCPClient::TCPClient() {
-    sock = -1;
+    socketClient = new Socket();
     port = 0;
     address = "";
 }
 
-bool TCPClient::setup(string address, int port) {
-    if (sock == -1) {
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock == -1) {
-            cout << "Error al inicializar el socket" << endl;
-        }
-    }
-    /*   if (inet_addr(address.c_str()) == -1) {
-           struct hostent *he;
-           struct in_addr **addr_list;
-           if ((he = gethostbyname(address.c_str())) == NULL) {
-               herror("gethostbyname");
-               cout << "Failed to resolve hostname\n";
-               return false;
-           }
-           addr_list = (struct in_addr **) he->h_addr_list;
-           for (int i = 0; addr_list[i] != NULL; i++) {
-               server.sin_addr = *addr_list[i];
-               break;
-           }
-       } else {
-            server.sin_addr.s_addr = inet_addr(address.c_str());
-       }*/
-    server.sin_addr.s_addr = inet_addr(address.c_str());
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        perror("connect failed. Error");
+bool TCPClient::setup(string addressToConnect, int listenPort) {
+    Logger* logger = Logger::getInstance();
+    this->address = addressToConnect;
+    socketClient->create(logger);
+    if(!socketClient->connectTo(address, listenPort)){
+        cout <<"Connection error" << endl;
         return false;
     }
+    cout << "Connection succesfull" << endl;
     return true;
 }
 
-bool TCPClient::Send(string data) {
-    if (sock != -1) {
-        if (send(sock, data.c_str(), strlen(data.c_str()), 0) < 0) {
-            cout << "Send failed : " << data << endl;
-            return false;
-        }
-    } else
-        return false;
-    return true;
+bool TCPClient::Send(void* data, size_t size_data) {
+    return socketClient->sendData(data, size_data);
 }
 
 void* TCPClient::receive(int size) {
     void *buffer =  malloc(size);
     memset(buffer, 0, size);
-
-    //string reply;
-    if (recv(sock, buffer, size, 0) < 0) {
-        cout << "receive failed!" << endl;
-        return nullptr;
-    }
-    //buffer[size - 1] = '\0';
-    //reply = buffer;
+    this->socketClient->reciveData(buffer,size);
     return buffer;
 }
 
@@ -82,5 +48,6 @@ string TCPClient::read() {
 }
 
 void TCPClient::exit() {
-    close(sock);
+    //socketClient->close();
+    //delete socketClient;
 }
--- src/Socket.cpp
+++ src/Socket.cpp
@@ -6,84 +6,112 @@
  */
 
 #include "Socket.h"
+#include <sys/socket.h>
 
 const string ERROR = "ERROR";
 const string INFO = "INFO";
 const string DEBUG = "DEBUG";
 
-Socket::Socket()
-{
-	this->fd = 0;
-	this->port = 0;
+Socket::Socket() {
+    this->fd = 0;
+    this->port = 0;
 }
 
-bool Socket::create(Logger* logger)
-{
-	this->fd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET permite la comunicacion entre aplicaciones
-												//corriendose en hosts conectados por IP version 4
-							 //SOCK_STREAM es el tipo de socket: reliable, bidirectional, byte-stream
-	if (fd == -1)
-	{
-		std::string msgError = "Error al crear socket.";
-		cout << msgError << endl;
-		logger->log(msgError, ERROR);
-		return false;
-	}
-	return true;
-}
-
-bool Socket::bindToAddress(int port, Logger* logger) //cuidado con pasar sin referencia!!
-{
-	this->port = port;
-	struct sockaddr_in serverAddress;
-    memset(&serverAddress, 0, sizeof(struct sockaddr_in)); //Clear structure
-    serverAddress.sin_family = AF_INET; //IP Domain Adress
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddress.sin_port = htons(this->port);
-
-    if (bind(this->fd, (struct sockaddr *) &serverAddress, sizeof(struct sockaddr_in)) == -1)
-    {
-    	std::string msgError = "Error en apertura de conexion.";
+bool Socket::create(Logger *logger) {
+    this->fd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET permite la comunicacion entre aplicaciones
+    //corriendose en hosts conectados por IP version 4
+    //SOCK_STREAM es el tipo de socket: reliable, bidirectional, byte-stream
+    if (fd == -1) {
+        std::string msgError = "Error al crear socket.";
         cout << msgError << endl;
-        logger->log(msgError,ERROR);
+        logger->log(msgError, ERROR);
         return false;
     }
     return true;
 }
 
-int Socket::get_fd()
+bool Socket::bindToAddress(int port, Logger *logger) //cuidado con pasar sin referencia!!
 {
-	return this->fd;
+    this->port = port;
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(struct sockaddr_in)); //Clear structure
+    serverAddress.sin_family = AF_INET; //IP Domain Adress
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(this->port);
+
+    if (bind(this->fd, (struct sockaddr *) &serverAddress, sizeof(struct sockaddr_in)) == -1) {
+        std::string msgError = "Error en apertura de conexion.";
+        cout << msgError << endl;
+        logger->log(msgError, ERROR);
+        return false;
+    }
+    return true;
 }
 
-bool Socket::listenConnection(int maxConnections, Logger* logger)
-{
-	if(listen(this->fd,maxConnections)==-1)
-	{
-		std::string msgError = "Error al escuchar las conecciones entrantes.";
-		cout << msgError << endl;
-		logger->log(msgError,ERROR);
-		return false;
-	}
-	return true;
+int Socket::get_fd() {
+    return this->fd;
 }
 
-bool Socket::initialize(Logger* logger, int port, int maxConnections)
-{
-	return (this->create(logger) && this->bindToAddress(port,logger) &&
-			this->listenConnection(maxConnections,logger));
+bool Socket::listenConnection(int maxConnections, Logger *logger) {
+    if (listen(this->fd, maxConnections) == -1) {
+        std::string msgError = "Error al escuchar las conecciones entrantes.";
+        cout << msgError << endl;
+        logger->log(msgError, ERROR);
+        return false;
+    }
+    return true;
+}
+
+bool Socket::initialize(Logger *logger, int port, int maxConnections) {
+    return (this->create(logger) && this->bindToAddress(port, logger) &&
+            this->listenConnection(maxConnections, logger));
 
 }
 
-void Socket::acceptConnection(Socket* otherSocket, struct sockaddr_in *clientAddress, Logger* logger)
-{
-	socklen_t sosize = sizeof(*clientAddress);
-	this->fd = accept(otherSocket->get_fd(), (struct sockaddr *) &(*clientAddress), &sosize);
-	if(this->fd == -1)
-	{
-		std::string msgError = "Error al escuchar las conecciones entrantes.";
-				cout << msgError << endl;
-				logger->log(msgError,ERROR);
-	}
+void Socket::acceptConnection(Socket *otherSocket, struct sockaddr_in *clientAddress, Logger *logger) {
+    socklen_t sosize = sizeof(*clientAddress);
+    this->fd = accept(otherSocket->get_fd(), (struct sockaddr *) &(*clientAddress), &sosize);
+    if (this->fd == -1) {
+        std::string msgError = "Error al escuchar las conecciones entrantes.";
+        cout << msgError << endl;
+        logger->log(msgError, ERROR);
+    }
 }
+
+bool Socket::connectTo(string address, int listenPort) {
+
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr(address.data());
+    serverAddress.sin_port = htons(listenPort);
+    socklen_t serverSize = sizeof(serverAddress);
+
+    /*VERIFICACION DE ERRORES*/
+    if (connect(fd, (struct sockaddr *) &serverAddress, serverSize) == -1) {
+        string error = strerror(errno);
+        //cout << "Error al conectar con el servidor " << strerror(errno) << endl;
+        return false;
+    }
+    return true;
+}
+
+bool Socket::sendData(void *to_send, size_t size) {
+    send(this->fd, to_send, size, NULL);
+}
+
+bool Socket::reciveData(void* data, int size) {
+    int totalRecived = 0, lastQuantityRecived = 0;
+    while (totalRecived < size) {
+        lastQuantityRecived = recv(fd, data, size, 0);
+        if (lastQuantityRecived <= 0) {
+            data = nullptr;
+            return false;
+        } else
+            totalRecived += lastQuantityRecived;
+    }
+    return true;
+}
+
+
+
 
