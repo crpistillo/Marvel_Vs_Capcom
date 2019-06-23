1f854e977274459f6afd451b0a7e7c1b5bde750b
DonatoJP <judonato@fi.uba.ar>
2019-06-23 18:30:50 -0300

    ProjectileWolverineServer

---
+++ src/CharactersServer/Projectiles/ProjectileWolverineServer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by donatojp on 23/06/19.
+//
+
+#include "ProjectileWolverineServer.h"
+
+
+ProjectileWolverineServer::ProjectileWolverineServer() : ProjectileServer() {
+    lastSprite = 1; //6 maybe
+    lastHittingSprite = 5;
+
+}
---
+++ src/CharactersServer/Projectiles/ProjectileWolverineServer.h
@@ -0,0 +1,16 @@
+//
+// Created by donatojp on 23/06/19.
+//
+
+#ifndef MARVEL_VS_CAPCOM_PROJECTILEWOLVERINESERVER_H
+#define MARVEL_VS_CAPCOM_PROJECTILEWOLVERINESERVER_H
+
+#include "ProjectileServer.h"
+
+class ProjectileWolverineServer final : public ProjectileServer{
+    ProjectileWolverineServer();
+
+};
+
+
+#endif //MARVEL_VS_CAPCOM_PROJECTILEWOLVERINESERVER_H
