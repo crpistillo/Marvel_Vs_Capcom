43bb73592d6b02bd1b5c235c779bd0307e567958
DonatoJP <judonato@fi.uba.ar>
2019-06-23 18:26:12 -0300

    ProjectileWolverineClient

---
+++ src/CharactersClient/ProjectileWolverineClient.cpp
@@ -0,0 +1,11 @@
+//
+// Created by donatojp on 23/06/19.
+//
+
+#include "ProjectileWolverineClient.h"
+
+
+ProjectileWolverineClient::ProjectileWolverineClient(bool secondaryColor):ProjectileClient(secondaryColor){
+    mvcFilePath = "/MVC2_Wolverine_";
+    projectileFilePath = "images/wolverine/wolverine_projectile_";
+}
---
+++ src/CharactersClient/ProjectileWolverineClient.h
@@ -0,0 +1,19 @@
+//
+// Created by donatojp on 23/06/19.
+//
+
+#ifndef MARVEL_VS_CAPCOM_PROJECTILEWOLVERINECLIENT_H
+#define MARVEL_VS_CAPCOM_PROJECTILEWOLVERINECLIENT_H
+
+
+
+
+#include "ProjectileClient.h"
+
+class ProjectileWolverineClient final : public ProjectileClient {
+
+public:
+    ProjectileWolverineClient(bool secondaryColor);
+};
+
+#endif //MARVEL_VS_CAPCOM_PROJECTILEWOLVERINECLIENT_H
